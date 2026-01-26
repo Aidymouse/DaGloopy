4c5fdcec178969863ff38d7f5343fc938f5f3b01
Aidan <aidymouse4891@gmail.com>
2026-01-26 22:03:54 +1300

    back button on articles

--- js/markdown.js
+++ js/markdown.js
@@ -1,14 +1,12 @@
-
 /*
- * @param markdown_text (string) - 
+ * @param markdown_text (string) -
  */
 export const process_markdown = (markdown_text) => {
+  // TODO: get rid of prelim headings
 
-	// TODO: get rid of prelim headings
-	
-	const s = new showdown.Converter()
+  const s = new showdown.Converter();
 
-	const html = s.makeHtml(markdown_text)
+  const html = s.makeHtml(markdown_text);
 
-	return html
-}
+  return html;
+};
