cd6fe941cc5532be9d7b209d95711e6613bff494
Aidan <aidymouse4891@gmail.com>
2026-01-31 11:53:47 +1300

    sort out book styling

--- components/shelfbook.js
+++ components/shelfbook.js
@@ -1,3 +1,9 @@
+const series_color_map = {
+  "ruby crown": "#ff4000",
+  "royan empire": "#009933",
+  "the royan empire": "#009933",
+};
+
 export class ShelfBook extends HTMLElement {
   static observed_attributes = ["title", "date", "contenturl", "series"];
 
@@ -12,14 +18,24 @@ export class ShelfBook extends HTMLElement {
   attributeChangedCallback(attr, oldVal, newVal) {}
 
   render() {
+    const series = this.getAttribute("series");
+    const series_style = series_color_map[series]
+      ? `style="background-color: ${series_color_map[series]}"`
+      : "";
+
     return `<article class="book-container">
                 <article class="book" 
 		      ${this.getAttribute("contenturl") ? 'onpointerup="book_pointerup(event)"' : ""}
 		      data-contenturl="${this.getAttribute("contenturl") ?? ""}"
 		      data-title="${this.getAttribute("title") ?? ""}"
 		>
-                  <h2>${this.getAttribute("title") ?? ""}</h2>
+			<div class="grid-cell">
+                		<h2>${this.getAttribute("title") ?? ""}</h2>
+			</div>
+		  <span class="book-strip" ${series_style}></span>
+		  <div class="grid-cell" style="justify-content: flex-end; padding-right: 0.5em;">
                   <h3>${this.getAttribute("date") ?? ""}</h3>
+		  </div>
                 </article>
               </article>`;
   }
