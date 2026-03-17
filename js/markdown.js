179cd70b7fc30fe10b98ebd3e0dab3176373df1b
Aidan Habedank <aidan.habedank@iotventures.net>
2026-03-18 12:08:01 +1300

    clean ... out of markdown so it doesnt cause gross html errors

--- js/markdown.js
+++ js/markdown.js
@@ -4,9 +4,11 @@
 export const process_markdown = (markdown_text) => {
   // TODO: get rid of prelim headings
 
+  const cleaned = markdown_text.replace(/\.\.\./g, "&hellip;")
+
   const s = new showdown.Converter({ tables: true });
 
-  const html = s.makeHtml(markdown_text);
+  const html = s.makeHtml(cleaned);
 
   return html;
 };
